1ccdc967de9858cf2369a2164ac650c280d5d020
Nada83Nikolic <nada83nikolic@gmail.com>
2022-02-15 19:10:38 +0100

    added js

--- src/pages/BasicPage.java
+++ src/pages/BasicPage.java
@@ -1,12 +1,15 @@
 package pages;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 public abstract class BasicPage {
 	protected WebDriver driver;
+	protected JavascriptExecutor js;
 
-	public BasicPage(WebDriver driver) {
+	public BasicPage(WebDriver driver, JavascriptExecutor js) {
 		this.driver = driver;
+		this.js=js;
 	}
 	
 
