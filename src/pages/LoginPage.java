a14122b731c141d6e54fad4ea85d64b23cd19dca
Nada83Nikolic <nada83nikolic@gmail.com>
2022-02-16 17:16:49 +0100

    LoginPage

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -1,5 +1,34 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage extends BasicPage {
+
+	public LoginPage(WebDriver driver, JavascriptExecutor js) {
+		super(driver, js);
+	}
+
+	public WebElement getUsername() {
+		return this.driver.findElement(By.name("username"));
+	}
+
+	public WebElement getPassword() {
+		return this.driver.findElement(By.name("password"));
+	}
+
+	public WebElement getLoginButton() {
+		return this.driver.findElement(By.name("btn_submit"));
+	}
+
+	public void login(String email, String password) {
+		this.getUsername().clear();
+		this.getUsername().sendKeys(email);
+		this.getPassword().clear();
+		this.getPassword().sendKeys(password);
+		this.getLoginButton().click();
+	}
 
 }
