a1f4553e4adc27db4acb201c6c7e786ca3e228e2
Nada83Nikolic <nada83nikolic@gmail.com>
2022-02-17 20:15:34 +0100

    AuthPage

--- src/pages/AuthPage.java
+++ src/pages/AuthPage.java
@@ -1,5 +1,31 @@
 package pages;
 
-public class AuthPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+public class AuthPage extends BasicPage {
+
+	public AuthPage(WebDriver driver, JavascriptExecutor js, WebDriverWait wait) {
+		super(driver, js, wait);
+	}
+
+	public WebElement getAccountDropDoown() {
+		return this.driver.findElement(By.xpath("//*[@class='filled ']/a"));
+	}
+
+	public WebElement getMyAccountField() {
+		return this.driver.findElement(By.linkText("My Account"));
+	}
+
+	public WebElement getLogOutField() {
+		return this.driver.findElement(By.linkText("Logout"));
+	}
+
+	public void logOut() {
+		this.getAccountDropDoown().click();
+		this.getLogOutField().click();
+	}
 }
